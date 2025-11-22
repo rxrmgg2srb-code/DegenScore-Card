b9abeeafca2ccfa99c8300350a16ee471570cee3
rxrmgg2srb-code <rxrmgg2srb@privaterelay.appleid.com>
2025-11-22 17:29:38 +0100

    Update formidable.d.ts

--- formidable.d.ts
+++ formidable.d.ts
@@ -1,7 +1,26 @@
 declare module 'formidable' {
+  import { IncomingMessage } from 'http';
+  
+  export interface Fields {
+    [key: string]: string | string[];
+  }
+  
+  export interface Files {
+    [key: string]: File | File[];
+  }
+  
+  export interface File {
+    size: number;
+    path: string;
+    name: string;
+    type: string;
+    lastModifiedDate?: Date;
+    hash?: string;
+  }
+  
   export class IncomingForm {
     constructor(options?: any);
-    parse(req: any, callback: (err: any, fields: any, files: any) => void): void;
+    parse(req: IncomingMessage, callback: (err: any, fields: Fields, files: Files) => void): void;
     uploadDir: string;
     keepExtensions: boolean;
     maxFileSize: number;
